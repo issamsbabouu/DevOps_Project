701693f0743cc6fc9d9b5e8202e0764d401bd0d9
spapowW <uirandonnee@gmail.com>
2024-11-15 09:33:58 +0100

    issam

---
+++ DevOps_Project/src/main/java/ma/ac/uir/DevOps_Project/DevOpsProjectApplication.java
@@ -0,0 +1,13 @@
+package ma.ac.uir.DevOps_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsProjectApplication.class, args);
+	}
+
+}
---
+++ DevOps_Project/src/test/java/ma/ac/uir/DevOps_Project/DevOpsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ac.uir.DevOps_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
